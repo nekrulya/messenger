2a21a88179c061b729e0d90177a2250212e86a70
nekrulya <nekrulyaa@mail.ru>
2024-10-31 12:19:07 +0500

    Настроил миграции (ура!)

--- src/database.py
+++ src/database.py
@@ -3,9 +3,13 @@ from sqlalchemy.orm import DeclarativeBase
 from src.config import settings
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
+
 engine = create_async_engine(url=settings.database.url)
 
-Base = DeclarativeBase()
+class Base(DeclarativeBase):
+    pass
+
+from src.auth.models import *
 
 async def create_tables():
     try:
