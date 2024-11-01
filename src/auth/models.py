b1f1951c49a5effd7d0e6974a8ebc796e1b40947
nekrulya <nekrulyaa@mail.ru>
2024-11-01 15:59:09 +0500

    Добавил часть схем pydantic для пользователей

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,3 +1,4 @@
+from sqlalchemy import BigInteger
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.database import Base
@@ -6,7 +7,7 @@ from src.database import Base
 class User(Base):
     __tablename__ = 'users'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
     hashed_password: Mapped[str]
     tg_id: Mapped[int] = mapped_column(unique=True)
--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -6,15 +6,21 @@ from pydantic import BaseModel, Field, ConfigDict
 class UserBase(BaseModel):
     username: Annotated[str, Field(..., min_length=3, max_length=20)]
 
-
     config = ConfigDict(from_attributes=True)
 
-class UserRead(UserBase):
+
+class UserReadRequest(UserBase):
     pass
 
-class UserCreate(UserBase):
+
+class UserReadResponse(UserBase):
+    pass
+
+
+class UserCreateRequest(UserBase):
     password: Annotated[str, Field(..., min_length=3, max_length=20)]
     tg_id: Annotated[int, Field(..., gt=0)]
 
 
-
+class UserCreateResponse(UserBase):
+    pass
